f3694637a6f0982b2a670b904f20f1bd88ae5c60
Ben Young <the12thbenyoung@gmail.com>
2019-07-16 22:43:29 -0500

    add another tube crop and iterative matrix thresholding

--- qr/findTubes.py
+++ qr/findTubes.py
@@ -10,38 +10,59 @@ SHOW_IMAGES = False
 #max number of pixels of different between y coordinates for tubes to be considered in same row
 SAME_ROW_THRESHOLD = 70
 
+#number of rows and columns in rack
 NUM_ROWS = 8
 NUM_COLS = 12
 
+#for cropping rack from original image
 RACK_THRESH_FACTOR = 0.75
-TUBES_THRESH_FACTOR = 0.45
+#for cropping tube area (tube+rims/shadows) from rack
+TUBE_AREA_THRESH_FACTOR = 0.4
+#for cropping just circular tube from tube area
+TUBE_THRESH_FACTOR = 0.82
+#for cropping data matrix from harris corner heatmap image
 HARRIS_THRESH_FACTOR = 0.01
 
-HARRIS_BLOCK_SIZE = 6
+#size of box used in harris corner algorithm
+HARRIS_BLOCK_SIZE = 9
 
+#typical edge lengths of contour bounding boxes of tube areas
 EDGE_LOWER_BOUND = 90
 EDGE_UPPER_BOUND = 190
 
-#should be: 80
+#smallest expected area of a contour containing a matrix in the harris image
+MIN_MATRIX_CONTOUR_AREA = 4000
 
 def show_image_small(name, img):
     cv2.imshow(name, cv2.resize(img, (int(img.shape[1]/5), int(img.shape[0]/5)), interpolation=cv2.INTER_AREA))
     cv2.waitKey(0)
 
-def crop_rect(img, mask, threshFactor):
-    x, thr = cv2.threshold(mask, threshFactor * mask.max(), 255, cv2.THRESH_BINARY)
-    thr = thr.astype('uint8')
+def outputData(filename, dataIndices):
+    with open(filename, 'w') as file:
+        letters = 'ABCDEFGH'
+        # file.write('row,col,value\n')
+        for row in dataIndices:
+            file.write('{},{},{}\n'.format(letters[row[0][1]-1],row[0][0],int(row[1])))
+
+#threshold image and find largest area contour
+def find_largest_contour(img, threshFactor):
+    x, thr = cv2.threshold(img, threshFactor * img.max(), 255, cv2.THRESH_BINARY)
 
     if SHOW_IMAGES:
         cv2.imshow('thr', thr)
 
     #get outer contour of data matrix
+    thr = thr.astype('uint8')
     contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     areas = list(map(lambda x: cv2.contourArea(cv2.convexHull(x)), contours))
-    max_i = np.argmax(areas)
+    max_index = np.argmax(areas)
 
-    rect = cv2.minAreaRect(contours[max_i])
+    return contours[max_index]
+
+#find the minAreaRect around the largest light object in img, rotate and crop
+def crop_smallest_rect(img, contour):
+    rect = cv2.minAreaRect(contour)
 
     # get the parameter of the small rectangle
     center, size, angle = rect[0], rect[1], rect[2]
@@ -67,14 +88,31 @@ def crop_rect(img, mask, threshFactor):
 
     return img_crop
 
+#like crop_smallest_rect, but bounding rect isn't rotated and thus can be cropped directly
+#param edge_pix is the number of extra pixels to crop on each sied inside bounding rect
+def crop_bounding_rect(img, contour, edge_pix):
+    rect = cv2.boundingRect(contour)
+    x, y, w, h = rect
+
+    img_crop = img[y+edge_pix : y+h-edge_pix, x+edge_pix : x+w-edge_pix]
+    return img_crop
+
 def process_matrix(img, blockSize, threshFactor, i):
     #detect corners
     harris = cv2.cornerHarris(img, HARRIS_BLOCK_SIZE, 1, 0.00)
+  
+    contour = find_largest_contour(harris, threshFactor)
+    contourArea = cv2.contourArea(cv2.convexHull(contour)),
+
+    #if area is too small, threshFactor was too high (didn't find whole matrix),
+    #so lower it and try again
+    while contourArea[0] < MIN_MATRIX_CONTOUR_AREA and threshFactor > 0:
+        threshFactor -= 0.1
+        contour = find_largest_contour(harris, threshFactor)
+        contourArea = cv2.contourArea(cv2.convexHull(contour)),
 
     #crop out matrix from tube
-    matrix = crop_rect(img, harris, threshFactor)
-
-    # cv2.imwrite('images/tube{}.jpg'.format(i), matrix)
+    matrix = crop_smallest_rect(img, contour)
 
     #empty slots have weird aspect ratios (tall or wide) after they're processed. We 
     #can use this to filter out some of them
@@ -86,14 +124,17 @@ def process_matrix(img, blockSize, threshFactor, i):
         return None
 
 if __name__ == '__main__':
-    img = cv2.imread('tubesmissing.jpg')
+    img = cv2.imread('qrphone.jpg')
 
     #convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    #crop to just rack, threshold and invert to get tubes in white
-    rack = crop_rect(gray, gray, RACK_THRESH_FACTOR)
-    x, rack_thr = cv2.threshold(rack, TUBES_THRESH_FACTOR * rack.max(), 255, cv2.THRESH_BINARY)
+    rackContour = find_largest_contour(gray, RACK_THRESH_FACTOR)
+    #crop to just rack 
+    rack = crop_smallest_rect(gray, rackContour)
+
+    #threshold and invert to get tubes in white
+    x, rack_thr = cv2.threshold(rack, TUBE_AREA_THRESH_FACTOR * rack.max(), 255, cv2.THRESH_BINARY)
     rack_thr = 255 - rack_thr
 
     # show_image_small('rack_thr', rack_thr)
@@ -121,7 +162,13 @@ if __name__ == '__main__':
 
         #hopefully only the tube contours are this square and this big
         if all([EDGE_LOWER_BOUND < dim and EDGE_UPPER_BOUND > dim for dim in [w,h]]):
-            tube = rack[y:y+h, x:x+w]
+            #crop to bounding rect around contour
+            tubeArea = rack[y:y+h, x:x+w]
+
+            #crop again to just tube, trying to cut out surrounding shadow
+            tube_inv = 255 - tubeArea
+            tubeContour = find_largest_contour(tube_inv, TUBE_THRESH_FACTOR)
+            tube = crop_bounding_rect(tubeArea, tubeContour, 5)
 
             if SHOW_IMAGES:
                 cv2.imshow('tube', tube)
@@ -166,7 +213,6 @@ if __name__ == '__main__':
 
     #sort dataIndices by x coordinate, then y
     dataIndices.sort(key = (lambda x: x[0][0]*1000 + x[0][1]))
-
     print(dataIndices)
 
     print(i)
--- qr/test.py
+++ qr/test.py
@@ -4,11 +4,27 @@ import cv2
 
 from pylibdmtx.pylibdmtx import decode
 
-img = cv2.imread('images/tube25.jpg')
+img = cv2.imread('images/tube20.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# cv2.imshow('thr', thr)
-# cv2.waitKey(0)
+cv2.imshow('before', img)
 
-for i in range(100):
-    data = decode(img)
+neg = 255 - img
+x, thr = cv2.threshold(neg, 0.82 * neg.max(), 255, cv2.THRESH_BINARY)
 
+thr = thr.astype('uint8')
+
+#get outer contour of data matrix
+contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+areas = list(map(lambda x: cv2.contourArea(cv2.convexHull(x)), contours))
+max_i = np.argmax(areas)
+
+
+rect = cv2.boundingRect(contours[max_i])
+x, y, w, h = rect
+
+tube = img[y+5:y+h-5, x+5:x+w-5]
+
+cv2.imshow('after', tube)
+cv2.waitKey(0)
